2331bb94d7114fe9e8d19641a82b5769459f3611
ASHFAQ ISSAK <ashfaq.abdi@gmail.com>
2023-03-17 13:05:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  return (customerName = customerName.toUpperCase());
+}
+
+// console.log(upperCaseCustomerName())
+//the code above takes in the globally declared variable and uses it in a function to uppercase it
+
+function setBestCustomer(){
+    return bestCustomer = 'not bob' 
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob'
+}
+
+const LeastFavoriteCustomer = 'andrew'
+
+function changeLeastFavoriteCustomer(){
+   return  LeastFavoriteCustomer = 'ASHFAQ'
+}
+
